3ab5bc696668fd2f5af9511ddaa11a61d1fa1c22
SENIOR\Filipe.Tobias <filipe.tobias@senior.com.br>
2018-08-27 17:00:31 -0300

    Middleware de Autorização

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,11 +1,14 @@
 import {Request, Response} from 'express';
 import {User, users} from './users';
 
+import * as jwt from 'jsonwebtoken';
+
 export const handleAuthentication = (req: Request, resp: Response) => {
     const user: User = req.body;
     if(isValid(user)){
-        const dbUser: User = users[user.email];
-        resp.json({name: dbUser.name, email: dbUser.email});
+        const dbUser = users[user.email];
+        const token = jwt.sign({sub: dbUser.email, iss: 'meat-api'}, 'meat-api-password');
+        resp.json({name: dbUser.name, email: dbUser.email, accessToken: token});
     }else{
         resp.status(403).json({message: 'Dados Inválidos.'});
     }
@@ -14,7 +17,7 @@ export const handleAuthentication = (req: Request, resp: Response) => {
 function isValid(user: User): boolean{
     if (!user) {
         return false;
-    }
+    } 
     const dbUser = users[user.email]
     return dbUser !== undefined && dbUser.matches(user);
 }
