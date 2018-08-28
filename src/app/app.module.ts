a164bc25186239fd6f87f5afa70001f831037fda
SENIOR\Filipe.Tobias <filipe.tobias@senior.com.br>
2018-08-28 15:05:24 -0300

    Criando o Componente de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { LoginComponent } from './security/login/login.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
     ReviewsComponent,
     OrderSummaryComponent,
     NotFoundComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
