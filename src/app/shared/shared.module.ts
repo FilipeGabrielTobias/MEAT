ff3abe798f06768f9922f7578598863824e6132b
SENIOR\Filipe.Tobias <filipe.tobias@senior.com.br>
2018-08-29 10:31:22 -0300

    Protegendo o Módulo de Compra com CanLoad

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,6 +10,7 @@ import { OrderService } from "../order/order.service";
 import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 import { NotificationService } from "./messages/notification.service";
 import { LoginService } from "../security/login/login.service";
+import { LoggedInGuard } from "../security/loggedin.guard";
 
 @NgModule({
     declarations: [
@@ -41,7 +42,8 @@ export class SharedModule {
                 RestaurantsService,
                 OrderService,
                 NotificationService,
-                LoginService
+                LoginService,
+                LoggedInGuard
             ]
         }
     }
